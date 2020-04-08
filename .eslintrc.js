7de1c46c3ccc6eaf8bc507d821dc530cf39d0ee7
Festus Omole <festusomole14@gmail.com>
2020-04-08 16:44:22 +0100

    Finished stage one

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   rules: {
     "comma-dangle": ["error", "never"],
-    "no-param-reassign": ["error", { "props": false }]
+    "no-param-reassign": ["error", { "props": false }],
+    'linebreak-style': ["error", "windows"]
   },
 };
--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,116 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint-disable operator-linebreak */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+/* eslint-disable no-param-reassign */
+const inputData = {
+    region: {
+        name: 'Africa',
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+    },
+    periodType: 'days',
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+};
+
+const convertToDays = (periodType, time) => {
+    switch (periodType) {
+        case 'weeks':
+            time *= 7;
+            break;
+        case 'months':
+            time *= 30;
+            break;
+        default:
+            time *= 1;
+            break;
+    }
+    return time;
+};
+
+const estimateInfectedAfterDays = (infected, periodType, time) => {
+    const timeInDays = convertToDays(periodType, time);
+    const daysFactor = timeInDays / 3;
+    const estimatedAfterDays = infected * 2 ** daysFactor;
+    return estimatedAfterDays;
+};
+
+const estimateHospitalBedsByTime = (severeCases, totalBeds) => {
+    const availableBeds = 0.35 * totalBeds;
+    if (availableBeds >= severeCases) return availableBeds;
+    return availableBeds - severeCases;
+};
+
+const estimateDollarsInFlight = (infected, avgIncome, periodType, time) => {
+    const timeInDays = convertToDays(periodType, time);
+    return infected * 0.65 * avgIncome * timeInDays;
+};
+
+const getImpactData = (data) => {
+    const impact = {};
+    impact.currentlyInfected = data.reportedCases * 10;
+    impact.infectionsByRequestedTime = estimateInfectedAfterDays(
+        impact.currentlyInfected,
+        data.periodType,
+        data.timeToElapse
+    );
+    impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
+    impact.hospitalBedsByRequestedTime = estimateHospitalBedsByTime(
+        impact.severeCasesByRequestedTime,
+        data.totalHospitalBeds
+    );
+    impact.casesForICUByRequestedTime = 0.05 * impact.infectionsByRequestedTime;
+    impact.casesForVentilatorsByRequestedTime =
+        0.02 * impact.infectionsByRequestedTime;
+    impact.dollarsInFlight = estimateDollarsInFlight(
+        impact.infectionsByRequestedTime,
+        data.region.avgDailyIncomeInUSD,
+        data.periodType,
+        data.timeToElapse
+    );
+    return impact;
+};
+
+const getSevereImpactData = (data) => {
+    const severeImpact = {};
+    severeImpact.currentlyInfected = data.reportedCases * 50;
+    severeImpact.infectionsByRequestedTime = estimateInfectedAfterDays(
+        severeImpact.currentlyInfected,
+        data.periodType,
+        data.timeToElapse
+    );
+    severeImpact.severeCasesByRequestedTime =
+        0.15 * severeImpact.infectionsByRequestedTime;
+    severeImpact.hospitalBedsByRequestedTime = estimateHospitalBedsByTime(
+        severeImpact.severeCasesByRequestedTime,
+        data.totalHospitalBeds
+    );
+    severeImpact.casesForICUByRequestedTime =
+        0.05 * severeImpact.infectionsByRequestedTime;
+    severeImpact.casesForVentilatorsByRequestedTime =
+        0.02 * severeImpact.infectionsByRequestedTime;
+    severeImpact.dollarsInFlight = estimateDollarsInFlight(
+        severeImpact.infectionsByRequestedTime,
+        data.region.avgDailyIncomeInUSD,
+        data.periodType,
+        data.timeToElapse
+    );
+
+    return severeImpact;
+};
+
+const covid19ImpactEstimator = (data) => {
+    const impact = getImpactData(data);
+    const severeImpact = getSevereImpactData(data);
+
+    return { data, impact, severeImpact };
+};
+
+// console.log(covid19ImpactEstimator(inputData));
 
 export default covid19ImpactEstimator;
