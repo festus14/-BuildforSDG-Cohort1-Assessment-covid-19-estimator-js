e8940bc32c1847a4d4f4913246e5ed1f16fb17ae
Festus Omole <festusomole14@gmail.com>
2020-04-08 22:22:05 +0100

    fixed some issues

--- src/estimator.js
+++ src/estimator.js
@@ -130,20 +130,20 @@ const covid19ImpactEstimator = (data) => {
   return { data, impact, severeImpact };
 };
 
-const inputData = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'days',
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-};
+// const inputData = {
+//   region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//   },
+//   periodType: 'days',
+//   timeToElapse: 58,
+//   reportedCases: 674,
+//   population: 66622705,
+//   totalHospitalBeds: 1380614
+// };
 
-console.log(covid19ImpactEstimator(inputData));
+// console.log(covid19ImpactEstimator(inputData));
 
-// export default covid19ImpactEstimator;
+export default covid19ImpactEstimator;
