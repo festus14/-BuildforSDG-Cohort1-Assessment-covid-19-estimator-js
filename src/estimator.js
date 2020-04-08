812a64291db735544aafcd18b819b7db95fd43b4
Festus Omole <festusomole14@gmail.com>
2020-04-08 20:29:40 +0100

    some reverted changes and rebase to last correct commit

--- src/estimator.js
+++ src/estimator.js
@@ -123,8 +123,8 @@ const covid19ImpactEstimator = (data) => {
 //     avgDailyIncomeInUSD: 5,
 //     avgDailyIncomePopulation: 0.71
 //   },
-//   periodType: 'months',
-//   timeToElapse: 19,
+//   periodType: 'days',
+//   timeToElapse: 58,
 //   reportedCases: 674,
 //   population: 66622705,
 //   totalHospitalBeds: 1380614
