4d57ec520423ca8aefa5b2595196c7ed7229478c
Festus Omole <festusomole14@gmail.com>
2020-04-09 13:18:56 +0100

    adjusted floor method

--- src/estimator.js
+++ src/estimator.js
@@ -26,8 +26,8 @@ const estimateInfectedAfterDays = (infected, periodType, time) => {
 };
 
 const estimateHospitalBedsByTime = (severeCases, totalBeds) => {
-  const availableBeds = 0.35 * totalBeds;
-  return Math.floor(availableBeds - severeCases);
+  const availableBeds = Math.floor(0.35 * totalBeds);
+  return availableBeds - severeCases;
 };
 
 const estimateDollarsInFlight = (infected, avgIncome, periodType, time) => {
@@ -35,7 +35,7 @@ const estimateDollarsInFlight = (infected, avgIncome, periodType, time) => {
   return Math.floor(infected * 0.65 * avgIncome * timeInDays);
 };
 
-const severeCasesByTime = (infected) => 0.15 * infected;
+const severeCasesByTime = (infected) => Math.floor(0.15 * infected);
 
 const casesForICUByTime = (infections) => {
   const val = Math.round(0.05 * infections);
