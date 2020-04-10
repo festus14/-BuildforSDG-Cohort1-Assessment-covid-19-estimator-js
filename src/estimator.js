99c74e27b5f8d9dc5b50a3ef698ab3f211fea92a
Festus Omole <festusomole14@gmail.com>
2020-04-10 22:25:16 +0100

    Added the front end url

--- src/estimator.js
+++ src/estimator.js
@@ -128,18 +128,4 @@ const covid19ImpactEstimator = (data) => {
   return { data, impact, severeImpact };
 };
 
-// const inputData = {
-//   region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 5,
-//     avgDailyIncomePopulation: 0.71
-//   },
-//   periodType: 'days',
-//   timeToElapse: 58,
-//   reportedCases: 674,
-//   population: 66622705,
-//   totalHospitalBeds: 1380614
-// };
-
 export default covid19ImpactEstimator;
